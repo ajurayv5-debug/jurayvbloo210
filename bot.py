1db33858541f61a39b89a05f23d5a74f9acb8afe
Yami sukihero <ajurayv5@gmail.com>
2026-06-16 13:18:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,17 +11,17 @@ def get_message():
     json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return "OK", 200
+    return "!", 200
 
 @server.route("/")
 def index():
     return "Bot ishlamoqda!"
 
+# Webhookni bir marta o'rnatish
 if __name__ == "__main__":
-    # Render avtomatik beradigan PORT o'zgaruvchisidan foydalanamiz
-    port = int(os.environ.get('PORT', 5000))
-    # Webhookni o'rnatish
     bot.remove_webhook()
-    # URL manzilingizni yozing (Render'dagi link)
-    bot.set_webhook(url='https://sizning-saytingiz.onrender.com/' + TOKEN)
-    server.run(host="0.0.0.0", port=port)
+    # URL: Render sizga bergan havola (masalan: https://anime-bot.onrender.com/)
+    # O'z manzilingizni shunga moslab yozing:
+    bot.set_webhook(url='https://YOUR-RENDER-APP-NAME.onrender.com/' + TOKEN)
+    
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
